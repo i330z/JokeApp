0ec3e58815261efaac30433662d22f5bb8b9d714
i330z <izzazahmed134@gmail.com>
2019-10-02 01:51:11 +0530

    first init

---
+++ components/Joke.vue
@@ -0,0 +1,22 @@
+<template>
+    <n-link :to="'jokes/' + id">
+        <div class="joke">
+            <p>{{ joke }}</p>
+        </div>
+    </n-link>
+</template>
+
+<script>
+export default {
+    name: 'Joke',
+    props: ['joke', 'id']
+}
+</script>
+
+<style scoped>
+.joke{
+    padding: 1rem;
+    border: 1px dotted black;
+    margin: 1rem 0rem;
+}
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="container">
+    <AppHeader />
+    <nuxt />
+  </div>
+</template>
+
+<script>
+import AppHeader from "../components/AppHeader";
+export default {
+  components:{
+    AppHeader
+  }
+}
+</script>
+
+<style>
+
+
+*,
+*:before,
+*:after {
+  box-sizing: border-box;
+  margin: 0;
+}
+
+a{
+  color: #666;
+  text-decoration: none;
+}
+
+ul{
+  list-style: none;
+}
+
+.container{
+  max-width: 800px;
+  margin: 2rem auto;
+  overflow: hidden;
+  padding: 1rem 2rem;
+  background: #ffff;
+}
+</style>
